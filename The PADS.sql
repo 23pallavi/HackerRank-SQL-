f871d5a19d68dd6372626e882a68a49f1628d14d
Pallavi <45979276+23pallavi@users.noreply.github.com>
2019-01-30 22:01:56 -0500

    Add files via upload

---
+++ The PADS.sql
@@ -0,0 +1,4 @@
+/* MySQL Solution */
+
+SELECT CONCAT(Name, '(', LEFT(Occupation,1),')') FROM OCCUPATIONS ORDER BY Name;
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.') FROM OCCUPATIONS GROUP BY Occupation ORDER BY COUNT(Occupation), Occupation;
